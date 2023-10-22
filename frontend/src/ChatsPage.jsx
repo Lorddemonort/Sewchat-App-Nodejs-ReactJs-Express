cef9156969228dea0d46cd67517e140fe2274689
SHAFIN SHAIKH <shafinshaikh25@gmail.com>
2023-10-22 22:12:44 +0530

    Initial commit

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
+
+const ChatsPage = (props) => {
+    return (
+        <div style={{ height: '100vh' }}>
+            <PrettyChatWindow 
+                projectId="0aaeed6e-628a-436f-a446-3c9cfeb19c68"
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100%' }}
+            />
+        </div>
+    )
+}
+
+export default ChatsPage
